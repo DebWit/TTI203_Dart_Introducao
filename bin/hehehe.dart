d6a14a5bfe0bf62bf7b31242e46d33496a42ea23
DebWit <deborawit@gmail.com>
2023-09-18 07:28:12 -0300

    hehehe

---
+++ bin/hehehe.dart
@@ -0,0 +1,24 @@
+// import 'package:hehehe/hehehe.dart' as hehehe;
+// // criar um aplicativo de lista de tarefas simples. Este aplicativo permitirá que os usuários adicionem, 
+// //visualizem e excluam tarefas da lista.
+ 
+
+//  // 1. criar listas
+// bool cListaOk = true;
+// while(var cListaOk){
+
+// }
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${hehehe.calculate()}!');
+// }
+
+void main(){
+  var filme = {
+    'titulo': 'Titanic',
+    'ano': 1999
+  };
+
+  var ano = filme['ano'] as String;
+  print(ano);
+}
