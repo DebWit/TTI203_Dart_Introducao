13bc406f1bfc6085f01e9c568facb98db6406a2a
DebWit <deborawit@gmail.com>
2023-09-18 07:24:06 -0300

    Create ex_01.dart

---
+++ ex_01.dart
@@ -0,0 +1,8 @@
+import "dart:io";
+import "dart:math";
+
+
+// 1. Calcule a área de um círculo com um raio de 5. 
+//(Use a fórmula da área do círculo: πr²).
+num raio = stdin.readLineSync();
+num circulo =  * (raio) * (raio)
